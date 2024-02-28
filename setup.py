3785c297ce801a687668aaff9bea7700c5e9bcca
LapisMYT <nikitagavrilin005@gmail.com>
2024-02-28 23:58:22 +0300

    .

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ if __name__ == "__main__":
     setup(
         name = "stonfi",
         version = "0.1",
-        packages = find_packages(include=["stonfi", "stonfi.*"])
+        packages = find_packages(include=["stonfi", "stonfi.*"]),
+        install_requires = [
+            "requests",
+            "tonsdk",
+            "tvm_valuetypes"
+        ]
     )
