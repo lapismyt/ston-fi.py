0589ac033ce8bab0694dd0a7da9d1d98c5848d4b
LapisMYT <nikitagavrilin005@gmail.com>
2024-02-28 23:56:31 +0300

    .

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        name = "stonfi",
+        version = "0.1",
+        packages = find_packages(include=["stonfi", "stonfi.*"])
+    )
