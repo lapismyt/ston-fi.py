d5ae5a51bbef0f83e038c49772a38cbe05da485b
Nikita Gavrilin <nikitagavrilin005@gmail.com>
2024-05-13 15:52:03 +0300

    utils

--- stonfi/utils.py
+++ stonfi/utils.py
@@ -1,8 +1,8 @@
-from stonfi import HTTPAPI
+from stonfi import HTTPAPI, PoolV1
 import asyncio
-from pytoniq import Address
+from pytoniq import Address, LiteClientLike
 
-async def get_jetton_decimals(jetton_address: str | Address) -> int:
+async def get_jetton_decimals(jetton_address: str | Address) -> int | None:
     http_api = HTTPAPI()
     jetton_address = jetton_address.to_str() if isinstance(jetton_address, Address) else jetton_address
     assets = await http_api.get_assets()
@@ -10,3 +10,14 @@ async def get_jetton_decimals(jetton_address: str | Address) -> int:
         if asset['contract_address'] == jetton_address:
             return asset['decimals']
     return None
+
+async def get_pool_readiness(pool: PoolV1, provider: LiteClientLike):
+    state = await provider.get_account_state(pool.address)
+    if not state.is_active():
+        return False
+    data = await pool.get_data()
+    for x in ['reserve0', 'reserve1']:
+        if not data[x] > 0:
+            return False
+    return True
+
